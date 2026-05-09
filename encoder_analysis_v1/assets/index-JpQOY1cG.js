70a20a7a795e68f23dd787114e5965d3cc05465c
jacobeverist <jacob.everist@gmail.com>
2026-05-09 13:07:37 -0700

    changed encoder_analysis_v1 base paths to relative without doing a rebuild

--- encoder_analysis_v1/assets/index-JpQOY1cG.js
+++ encoder_analysis_v1/assets/index-JpQOY1cG.js
@@ -29,7 +29,7 @@ var require_stdin = __commonJS({
           fetch(s.href, a);
         }
       })();
-      const Nt = "/dcc-public/encoder_analysis/assets/dcc_bundle_bg-XCii5kzL.wasm", Ft = async (e = {}, t) => {
+      const Nt = ".//assets/dcc_bundle_bg-XCii5kzL.wasm", Ft = async (e = {}, t) => {
         let n;
         if (t.startsWith("data:")) {
           const o = t.replace(/^data:.*?base64,/, "");
